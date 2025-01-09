a2b60ad3ad428a477d5887b3408a1be35bb8630f
sattorov <sattorov.ahu@gmail.com>
2025-01-09 21:25:42 +0500

    added xlsx file creator

--- handlers/users/read_exel.py
+++ handlers/users/read_exel.py
@@ -1,7 +1,11 @@
 import os
 from aiogram import types, Router, F
 from aiogram.fsm.context import FSMContext
-from utils.misc.assistants import send_products_xlsx, check_file_xlsx, get_user_lang
+from keyboards.default.main import main_button
+from utils.misc.assistants import (
+    send_products_xlsx, check_file_baseProduct_xlsx,
+    get_user_lang, check_file_Product_xlsx
+)
 
 router = Router()
 
@@ -22,24 +26,64 @@ async def download_file(message: types.Message, state: FSMContext):
             }.get(lang)
         )
         return
+    await message.bot.download_file(
+        file_path=file_path, destination=f"data/products/{message.from_user.id}_products.xlsx")
 
-    await message.bot.download_file(file_path=file_path,
-                                    destination=f"data/products/{message.from_user.id}_products.xlsx")
-    if not await check_file_xlsx(message.from_user.id):
+    btn = types.ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                types.KeyboardButton(text='Product'),
+                types.KeyboardButton(text='BaseProduct'),
+            ]
+        ], resize_keyboard=True)
+
+    await message.answer(
+        text={
+            'uz': "Malumot turini tanlang!",
+            'ru': "Выберите тип данных!"
+        }.get(lang),
+        reply_markup=btn
+    )
+
+
+@router.message(F.text.in_(['Product', 'BaseProduct']))
+async def create_product(message: types.Message, state: FSMContext):
+    lang = await get_user_lang(message=message, state=state)
+    if not lang:
+        return
+
+    if message.text == 'Product':
+        check_res = await check_file_Product_xlsx(message.from_user.id)
+    else:
+        check_res = await check_file_baseProduct_xlsx(message.from_user.id)
+
+    product_type = {'Product': 'product', 'BaseProduct': 'base_product'}.get(message.text)
+
+    if not check_res:
         await message.answer(
             text={
                 'uz': "Ushbu fayl berilgan shartlarga to'g'ri kelmadi.",
                 'ru': "Этот файл не соответствует указанным условиям."
-            }.get(lang)
+            }.get(lang),
+            reply_markup=await main_button(lang)
         )
         os.remove(f'data/products/{message.from_user.id}_products.xlsx')
         return
 
-    result = await send_products_xlsx(message.from_user.id)
+    await message.answer(
+        text={
+            'uz': "Malumotlar yuklanmoqda",
+            'ru': "Загрузка данных"
+        }.get(lang),
+        reply_markup=await main_button(lang)
+    )
+
+    result = await send_products_xlsx(message.from_user.id, type_=product_type)
     os.remove(f'data/products/{message.from_user.id}_products.xlsx')
     if result.status_code == 200:
         await message.answer(
-            text=f"{result.json().get('result')}"
+            text=f"{result.json().get('result')}",
+            reply_markup=await main_button(lang)
         )
         return
 
